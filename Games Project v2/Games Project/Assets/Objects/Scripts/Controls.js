8990a5be03c80644204e1c7a5d5d04d0ce45d4c3
KMDavis10 <davisk10@tcnj,edu>
2015-04-23 19:28:50 -0400

    Fixed inventory bug & brought in endgame scenario

--- Games Project v2/Games Project/Assets/Objects/Scripts/Controls.js
+++ Games Project v2/Games Project/Assets/Objects/Scripts/Controls.js
@@ -8,9 +8,9 @@ public class Controls extends MonoBehaviour
 	var Player: GameObject;
 	public static var levels: String[] = new String[18];
 	public static var inventoryArray: String[] = new String[18];
-	public static var henryMotiveArray = Array();
-	public static var randallMotiveArray = Array();
-	public static var veronicaMotiveArray = Array();
+	public static var henryMotiveArray: String[] = new String[3];
+	public static var randallMotiveArray: String[] = new String[3];
+	public static var veronicaMotiveArray: String[] = new String[3];
 	public static var listCount: int = 0;
 	public static var emptyCount: int = 0;
 	public static var maxActions: int = 0;
@@ -57,7 +57,7 @@ public class Controls extends MonoBehaviour
 				randallCam.transform.position = Vector3(68, 6, 180);
 				Veronica.transform.position = Vector3(100,0,20);
 				veronicaCam.transform.position = Vector3(99, 6, 23);
-				Player.transform.position = Vector3(80,10,80);
+				Player.transform.position = Vector3(20,10,80);
 			}
 		} 
 		else {
@@ -70,8 +70,8 @@ public class Controls extends MonoBehaviour
 				Randall.transform.position = Vector3(65,0,15);
 				randallCam.transform.position = Vector3(67, 6, 20);
 				Veronica.transform.position = Vector3(170,0,10);
-				veronicaCam.transform.position = Vector3(169, 6, 13);
-				Player.transform.position = Vector3(125,10,10);
+				veronicaCam.transform.position = Vector3(170, 7.5, 13);
+				Player.transform.position = Vector3(80,10,10);
 			}
 		}
 	}
@@ -79,10 +79,10 @@ public class Controls extends MonoBehaviour
 	function OnGUI() {
 		GUI.Box (new Rect(650, 300, 200, 25), "Action points remaining: " + remainingActions.ToString());
 
-		/*if (endGame) {
+		if (endGame) {
 			print1 = false;
 			/** If the user picks Henry as the killer*/
-			/*if (displayNPCs) { 
+			if (displayNPCs) { 
 				if (GUI.Button (new Rect (100, 280, 100, 20), "Henry") && displayNPCs) {
 					displayMotives = true;
 					correctNPC = true;
@@ -100,8 +100,8 @@ public class Controls extends MonoBehaviour
 				}
 			}
 			if (displayMotives) {
-				print (henryMotiveArray.Count);
-				for (int i = 0; i < henryMotiveArray.Count; i++) {
+				print (henryMotiveArray.Length);
+				for (var i = 0; i < henryMotiveArray.Length; i++) {
 					if (GUI.Button (new Rect (200, (50*i), 500, 20), henryMotiveArray [i]) && displayMotives) {
 						if (henryMotiveArray[i] == "I did it...what, you think I'm kidding?") {
 							displayEvidence = true;
@@ -117,7 +117,7 @@ public class Controls extends MonoBehaviour
 				}
 			}
 			if (displayEvidence) {
-				for (int j = 0; j < Inventory.Count; j++) {
+				for (var j = 0; j < Inventory.count; j++) {
 					if (inventoryArray[j].Equals ("empty") == false) {
 						if (GUI.Button (new Rect ((100 * j), 280, 100, 20), Inventory.inventoryItems[j]) && displayEvidence) {
 							if (Inventory.inventoryItems[j] == "knife") {
@@ -134,10 +134,10 @@ public class Controls extends MonoBehaviour
 			}
 
 			if (displayMotives2) {
-				print (veronicaMotiveArray.Count);
-				for (int i = 0; i < veronicaMotiveArray.Count; i++) {
-					if (GUI.Button (new Rect (200, (50*i), 500, 20), veronicaMotiveArray [i]) && displayMotives2) {
-						if (veronicaMotiveArray[i] == "I may or may not have an STD...") {
+				print (veronicaMotiveArray.Length);
+				for (var k = 0; k < veronicaMotiveArray.Length; k++) {
+					if (GUI.Button (new Rect (200, (50*k), 500, 20), veronicaMotiveArray [k]) && displayMotives2) {
+						if (veronicaMotiveArray[k] == "I may or may not have an STD...") {
 							displayEvidence2 = true;
 							correctMotive = 2;
 							displayMotives2 = false;
@@ -151,10 +151,10 @@ public class Controls extends MonoBehaviour
 				}
 			}
 			if (displayEvidence2) {
-				for (int j = 0; j < inventoryArray.Count; j++) {
-					if (Inventory.inventoryItems[j].Equals ("empty") == false) {
-						if (GUI.Button (new Rect ((100 * j), 280, 100, 20), Inventory.inventoryItems[j]) && displayEvidence2) {
-							if (inventoryArray [j] == "knife") {
+				for (var l = 0; l < inventoryArray.length; l++) {
+					if (Inventory.inventoryItems[l].Equals ("empty") == false) {
+						if (GUI.Button (new Rect ((100 * l), 280, 100, 20), Inventory.inventoryItems[l]) && displayEvidence2) {
+							if (inventoryArray [l] == "knife") {
 								correctMurderWeapon = 2;
 								displayEvidence2 = false;
 							}
@@ -167,10 +167,10 @@ public class Controls extends MonoBehaviour
 				}
 			}
 			if (displayMotives3) {
-				print (randallMotiveArray.Count);
-				for (int i = 0; i < randallMotiveArray.Count; i++) {
-					if (GUI.Button (new Rect (200, (50*i), 500, 20), randallMotiveArray [i]) && displayMotives3) {
-						if (randallMotiveArray[i] == "I hate the manager.") {
+				print (randallMotiveArray.Length);
+				for (var m = 0; m < randallMotiveArray.Length; m++) {
+					if (GUI.Button (new Rect (200, (50*m), 500, 20), randallMotiveArray [m]) && displayMotives3) {
+						if (randallMotiveArray[m] == "I hate the manager.") {
 							displayEvidence3 = true;
 							correctMotive = 2;
 							displayMotives3 = false;
@@ -184,10 +184,10 @@ public class Controls extends MonoBehaviour
 				}
 			}
 			if (displayEvidence3) {
-				for (int j = 0; j < inventoryArray.Count; j++) {
-					if (Inventory.inventoryItems[j].Equals ("empty") == false) {
-						if (GUI.Button (new Rect ((100 * j), 280, 100, 20), Inventory.inventoryItems[j]) && displayEvidence3) {
-							if (inventoryArray [j] == "knife") {
+				for (var n = 0; n < inventoryArray.length; n++) {
+					if (Inventory.inventoryItems[n].Equals ("empty") == false) {
+						if (GUI.Button(new Rect ((100 * n), 280, 100, 20), Inventory.inventoryItems[n]) && displayEvidence3) {
+							if (inventoryArray [n] == "knife") {
 								correctMurderWeapon = 2;
 								displayEvidence3 = false;
 							}
@@ -207,6 +207,6 @@ public class Controls extends MonoBehaviour
 				GUI.Box (new Rect (0, 0, Screen.width, Screen.height - 200), "You lost! Try again or die");
 			}
 
-		} */
-	}
+		}
+	} 
 }
